6878cc4a3551a097c1b26005b58f654214988846
Joshua Trono <Josh.trono@gmail.com>
2023-04-26 20:04:49 -0400

    added coverage and changed the test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,10 +7,7 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
 
-  SimpleCov.start 'rails' do
-    add_filter '/test/'
-    track_files '{app,lib}/**/*.rb'
-  end
+  SimpleCov.start 'rails'
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
